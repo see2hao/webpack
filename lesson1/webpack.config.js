3eb002c4d2866511dd218f989acff893bcad661e
see2hao <hulang1hao@163.com>
2018-07-02 14:07:33 +0800

    '多入口，多出口'

--- lesson1/webpack.config.js
+++ lesson1/webpack.config.js
@@ -2,7 +2,13 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './app.js',
+    //单入口
+    // entry: './app.js',
+    //多入口
+    entry: {
+        'entry/1/entry1': './1.js',
+        'entry2/2/entry2': './2.js',
+    },
     output:{
         //windows环境下，一定需要绝对路径
         path: path.resolve(__dirname, 'dist/'),
